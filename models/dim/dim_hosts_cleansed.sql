c7991aa5f04699cb7a1fc35d94a88788804cc060
kapoor98ak <kapoor98.ak@gmail.com>
2024-11-30 12:49:46 -0400

    Core Layer: dim models created from the src models

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,11 @@
+with dim_hosts_cleansed as
+(
+    select * from {{ ref("src_hosts")}}
+)
+
+select 
+    host_id,
+    NVL(host_name, 'Anonymous') as host_name,
+    IS_SUPERHOST, CREATED_AT, UPDATED_AT
+
+from dim_hosts_cleansed
---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,18 @@
+with src_listings as (
+    select * from {{ ref("src_listings")}}
+)
+
+select 
+    listing_id,
+    listing_name,
+    room_type,
+    case 
+        when MINIMUM_NIGHTS = 0 then 1
+        else MINIMUM_NIGHTS
+    end 
+    as MINIMUM_NIGHTS,
+    host_id,
+    replace(PRICE_STR, '$', '')::number(10,2) as PRICE,
+    CREATED_AT,
+    UPDATED_AT
+from src_listings
