abc975ca08625df8ca35c9483d2a7b8ac40bcb0e
kapoor98ak <kapoor98.ak@gmail.com>
2024-11-30 01:21:36 -0400

    Staging layer - src_listing model created

---
+++ models/src/src_listings.sql
@@ -0,0 +1,15 @@
+WITH raw_listing AS (
+    SELECT * FROM AIRBNB.RAW.RAW_LISTINGS
+)
+
+SELECT 
+    id AS listing_id,
+    name AS listing_name,
+    LISTING_URL,
+    ROOM_TYPE,
+    MINIMUM_NIGHTS,
+    PRICE AS PRICE_STR,
+    CREATED_AT,
+    UPDATED_AT
+FROM
+    raw_listing
