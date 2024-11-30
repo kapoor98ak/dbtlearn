ed611cc521b4588bc02503a7cf15d1e0bc7ded4e
kapoor98ak <kapoor98.ak@gmail.com>
2024-11-30 10:30:41 -0400

    Staging layer - src_hosts model created

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,13 @@
+with raw_hosts as (
+    select * from AIRBNB.RAW.RAW_HOSTS
+)
+
+select 
+    id as host_id,
+    name as host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+    
+from raw_hosts 
+    
